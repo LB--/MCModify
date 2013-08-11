096040e998abcc61eadab5358ff246a052c52621
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-08-10 22:20:09 -0500

    Add test script (unable to link)

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <limits>
+
 #include "NamedBinaryTag.hpp"
 
-int main()
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/copy.hpp>
+
+int main(int nargs, char const *const *args)
 {
+	if(nargs != 2) return 1;
+
+	std::ifstream compressed (args[1], std::ios::in|std::ios::binary);
+	if(!compressed) return 2;
+	std::stringstream decompressed;
+	boost::iostreams::filtering_istreambuf fis;
+	fis.push(boost::iostreams::gzip_decompressor());
+	fis.push(compressed);
+	boost::iostreams::copy(fis, decompressed);
+	decompressed.seekg(std::ios::beg);
+	auto nbt = NBT::Tag::read(decompressed);
+
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
